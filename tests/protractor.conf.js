50330153755f50d1cb2589c85196ef6b61c4de4d
Phong Huynh <phuynh@architech.ca>
2016-10-23 11:49:34 -0400

    Remove unused tests

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -4,31 +4,14 @@ exports.config = {
     capabilities: {
         browserName: 'chrome'
     },
-    /*
-    chromeOnly: true,
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: [
-                '--allow-file-access-from-files',
-                '--disable-cache',
-                '--disable-application-cache',
-                '--disable-offline-load-stale-cache',
-                '--disk-cache-size=0',
-                '--v8-cache-options=off'
-            ]
-        }
-    },
-    */
 
     jasmineNodeOpts: {
-        showColors: true, // Use colors in the command line report.
+        showColors: true,
     },
 
     onPrepare: function() {
         global.nav = browser.driver;
-        global.URL = 'http://localhost:9000/' // lembrar de levantar o servidor antes com o http-server
-        //global.URL = 'file:///C:/Users/gustavo.WDEV/Documents/design/camposAnexos.html'
+        global.URL = 'http://localhost:9000/'
     }
 
 }
